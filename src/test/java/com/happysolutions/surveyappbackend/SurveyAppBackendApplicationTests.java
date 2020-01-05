c1980677dd1261d99756ca12e7ba17db08fb6bec
harshgundecha <harsh.gundecha@gmail.com>
2020-01-05 06:23:35 +0530

    initiate springboot project

---
+++ src/test/java/com/happysolutions/surveyappbackend/SurveyAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.happysolutions.surveyappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
